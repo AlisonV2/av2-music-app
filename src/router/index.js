12f8106aba9ef77fcecc853c6d6a5d080f518a14
AlisonV2 <alison.vandromme007@gmail.com>
2021-08-21 12:33:17 +0200

    handled uploads and crud ops on songs

--- src/router/index.js
+++ src/router/index.js
@@ -36,7 +36,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-  linkExactActiveClass: 'text-yellow-500',
+  linkExactActiveClass: 'text-pink-500',
 });
 
 router.beforeEach((to, from, next) => {
