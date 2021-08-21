dc3b43ae72c8d9ecd35ff858e98f643124a947f8
AlisonV2 <alison.vandromme007@gmail.com>
2021-08-21 11:56:20 +0200

    added firebase and setup auth + persistance

--- src/App.vue
+++ src/App.vue
@@ -258,5 +258,8 @@ export default {
     AppHeader,
     AuthModal,
   },
+  created() {
+    this.$store.dispatch('init_login');
+  },
 };
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,20 +3,28 @@
   <header id="header">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#"><img height="70" width="70" src="../assets/img/logo.svg" /></a>
+      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">
+        <img height="70" width="70" src="../assets/img/logo.svg" />
+      </a>
 
-      <div class="flex flex-grow items-center">
+       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
-          <li>
+          <li v-if="!userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">
               Login / Register
             </a>
           </li>
-          <li>
-            <a class="px-2 text-white" href="#">Manage</a>
-          </li>
+          <template v-else>
+            <li>
+              <a class="px-2 text-white" href="#">Manage</a>
+            </li>
+            <li>
+              <a class="px-2 text-white" href="#"
+                @click.prevent="signout">Logout</a>
+            </li>
+          </template>
         </ul>
       </div>
     </nav>
@@ -24,12 +32,16 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
+import { mapMutations, mapState, mapActions } from 'vuex';
 
 export default {
   name: 'Header',
+  computed: {
+    ...mapState(['userLoggedIn']),
+  },
   methods: {
     ...mapMutations(['toggleAuthModal']),
+    ...mapActions(['signout']),
   },
 };
 </script>
