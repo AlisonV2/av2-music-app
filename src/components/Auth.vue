d907d32151b502cb7cd6ec36dcfd8fa4c03ec7c4
AlisonV2 <alison.vandromme007@gmail.com>
2021-08-21 19:27:17 +0200

    fixed form on mobile

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -11,8 +11,8 @@
       <!-- This element is to trick the browser into centering the modal contents. -->
       <span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
 
-      <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden
-        shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+      <div class="inline-block align-bottom bg-white opacity-90 rounded-lg text-left overflow-hidden
+        shadow-xl transform transition-all md:my-8 md:align-middle max-w-lg w-full">
 
         <!-- Add margin if you want to see some of the overlay behind the modal-->
         <div class="py-4 text-left px-6">
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,14 @@
 <template>
-  <!-- Header -->
-  <header id="header">
+    <!-- App Name -->
+  <nav class="p-2 mt-0 fixed w-full z-10 top-0">
+    <div class="container mx-auto flex justify-start items-center py-2 px-4">
       <!-- App Name -->
-<nav class="container mx-auto flex justify-start items-center py-5 px-4">
-      <!-- App Name -->
-      <router-link class="text-white font-bold uppercase text-2xl mr-4"
-        :to="{ name: 'home' }" exact-active-class="no-active">
-        <img height="70" width="70" src="../assets/img/logo.png" id="logo"/>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+      >
+        <img height="70" width="70" src="../assets/img/logo.png" id="logo" />
       </router-link>
 
       <div class="flex flex-grow justify-end">
@@ -19,7 +21,11 @@
             </router-link>
           </li> -->
           <li v-if="!userLoggedIn">
-            <a class="text-white nav-item" href="#" @click.prevent="toggleAuthModal">
+            <a
+              class="text-white nav-item"
+              href="#"
+              @click.prevent="toggleAuthModal"
+            >
               Login
             </a>
           </li>
@@ -30,37 +36,38 @@
               </router-link>
             </li>
             <li>
-              <a class="text-white nav-item" href="#"
-                @click.prevent="signout">Logout</a>
+              <a class="text-white nav-item" href="#" @click.prevent="signout"
+                >Logout</a
+              >
             </li>
           </template>
         </ul>
       </div>
+    </div>
     </nav>
-  </header>
 </template>
 
 <script>
-import { mapMutations, mapState } from 'vuex';
+  import { mapMutations, mapState } from 'vuex';
 
-export default {
-  name: 'Header',
-  computed: {
-    ...mapState({
-      userLoggedIn: (state) => state.auth.userLoggedIn,
-    }),
-  },
-  methods: {
-    ...mapMutations(['toggleAuthModal']),
-    signout() {
-      this.$store.dispatch('signout', {
-        router: this.$router,
-        route: this.$route,
-      });
-      if (this.$route.meta.requiresAuth) {
-        this.$router.push({ name: 'home' });
-      }
+  export default {
+    name: 'Header',
+    computed: {
+      ...mapState({
+        userLoggedIn: (state) => state.auth.userLoggedIn,
+      }),
     },
-  },
-};
+    methods: {
+      ...mapMutations(['toggleAuthModal']),
+      signout() {
+        this.$store.dispatch('signout', {
+          router: this.$router,
+          route: this.$route,
+        });
+        if (this.$route.meta.requiresAuth) {
+          this.$router.push({ name: 'home' });
+        }
+      },
+    },
+  };
 </script>
